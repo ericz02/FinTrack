3bdd4bede73927ec2570ce39a9af25707f3ab67e
ericz02 <ezheng0720@gmail.com>
2024-04-25 19:37:18 -0400

    init

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-blue-500 text-2xl'>hasdidas</h1>
+    </div>
+  )
+}
+
+export default App
