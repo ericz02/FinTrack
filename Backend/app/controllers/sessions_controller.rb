7012b30eb3e64f632d0fb3d72ebfd51abcad13dd
Jerry <78663777+Jerrry-10@users.noreply.github.com>
2024-04-25 22:39:04 -0400

    Backend: Logout

--- Backend/app/controllers/sessions_controller.rb
+++ Backend/app/controllers/sessions_controller.rb
@@ -3,17 +3,21 @@
 class SessionsController < ApplicationController
   # POST /login
   def create
-  user = User.find_by(email: params[:email])
-  if user
-    if user.authenticate(params[:password])
-      session[:user_id] = user.id
-      render json: { status: 'Logged in', user: user }
+    user = User.find_by(email: params[:email])
+    if user
+      if user.authenticate(params[:password])
+        session[:user_id] = user.id
+        render json: { status: 'Logged in', user: }
+      else
+        render json: { status: 'Error', message: 'Incorrect password' }, status: :unauthorized
+      end
     else
-      render json: { status: 'Error', message: 'Incorrect password' }, status: :unauthorized
+      render json: { status: 'Error', message: 'User not found' }, status: :not_found
     end
-  else
-    render json: { status: 'Error', message: 'User not found' }, status: :not_found
   end
-end
 
+  def destroy
+    session.delete(:user_id)
+    render json: { status: 'Logged out' }
+  end
 end
--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[create index show]
   post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
