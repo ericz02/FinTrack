92ccbec701e4238dccb803756437cb3e8ab6dbcd
Jerry <78663777+Jerrry-10@users.noreply.github.com>
2024-04-25 22:09:15 -0400

    Users post request

---
+++ Backend/app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+ 
+  resources :users, only: [:create]
+
+
 end
