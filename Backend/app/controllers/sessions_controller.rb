e20c8446898e20002b9523a7261a581e7972dc54
Jerry <78663777+Jerrry-10@users.noreply.github.com>
2024-04-25 22:34:35 -0400

    Backend: Login

--- Backend/app/controllers/sessions_controller.rb
+++ Backend/app/controllers/sessions_controller.rb
@@ -3,12 +3,17 @@
 class SessionsController < ApplicationController
   # POST /login
   def create
-    user = User.find_by(email: params[:email])
-    if user&.authenticate(params[:password])
+  user = User.find_by(email: params[:email])
+  if user
+    if user.authenticate(params[:password])
       session[:user_id] = user.id
-      render json: { status: 'Logged in', user: }
+      render json: { status: 'Logged in', user: user }
     else
-      render json: { status: 'Error', message: 'Invalid credentials' }, status: :unauthorized
+      render json: { status: 'Error', message: 'Incorrect password' }, status: :unauthorized
     end
+  else
+    render json: { status: 'Error', message: 'User not found' }, status: :not_found
   end
+end
+
 end
