3a83aadded923c0d7182b07a5ed323efdce017e9
Jerry <78663777+Jerrry-10@users.noreply.github.com>
2024-05-04 11:27:24 -0400

    Backend: User validation on email

--- Backend/app/controllers/sessions_controller.rb
+++ Backend/app/controllers/sessions_controller.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
   # POST /login
   def create
     user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
-        render json: { 
-            status: 'Logged in', 
-            user: { id: user.id, email: user.email, name: user.name } 
-        }
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: {
+        status: 'Logged in',
+        user: { id: user.id, email: user.email, name: user.name }
+      }
     elsif user
-        render json: { status: 'Error', message: 'Incorrect password' }, status: :unauthorized
+      render json: { status: 'Error', message: 'Incorrect password' }, status: :unauthorized
     else
-        render json: { status: 'Error', message: 'User not found' }, status: :not_found
+      render json: { status: 'Error', message: 'User not found' }, status: :not_found
     end
   end
 
