959283ff82b0b0fd52d63554bce2c740825495a4
Jerry <78663777+Jerrry-10@users.noreply.github.com>
2024-05-07 15:11:19 -0400

    Backend: adds more info to dashboard

--- Backend/app/controllers/dashboards_controller.rb
+++ Backend/app/controllers/dashboards_controller.rb
@@ -1,13 +1,18 @@
+# frozen_string_literal: true
+
 class DashboardsController < ApplicationController
   before_action :set_user
-  
+
   # GET /users/:user_id/dashboard
   def show
     @dashboard = Dashboard.new(@user)
     render json: {
       net_worth: @dashboard.net_worth,
-      total_balance: @dashboard.total_balance,
+      total_bank_balance: @dashboard.total_bank_balance,
       total_expenses: @dashboard.total_expenses,
+      total_transcations: @dashboard.total_transcations,
+      total_debt: @dashboard.total_debt,
+      total_savings: @dashboard.total_savings,
       financial_summary: @dashboard.financial_summary
     }
   end
@@ -18,6 +23,5 @@ class DashboardsController < ApplicationController
   def set_user
     @user = User.find(params[:user_id])
     # Add authorization check here if using a system like Pundit or ensure the user is the current user
-   
   end
 end
--- Backend/app/models/dashboard.rb
+++ Backend/app/models/dashboard.rb
@@ -1,4 +1,4 @@
-
+# frozen_string_literal: true
 
 class Dashboard
   attr_reader :user
@@ -9,17 +9,32 @@ class Dashboard
 
   # Calculate net worth by subtracting total expenses from total bank account balance
   def net_worth
-    total_balance - total_expenses
+    total_bank_balance - total_expenses
+  end
+
+  # Sum of all transcations from user's bank accounts
+  def total_transcations
+    user.transactions.sum(:amount)
   end
 
   # Sum of all balances from user's bank accounts
-  def total_balance
+  def total_bank_balance
     user.bank_accounts.sum(:balance)
   end
 
   # Sum of all user's expenses
   def total_expenses
     user.expenses.sum(:amount)
+  end
+
+  # Sum of all debt's expenses
+  def total_debt
+    user.debts.sum(:amount)
+  end
+
+  # Sum of all budgets
+  def total_savings
+    user.budgets.sum(:savings_goal)
   end
 
   # List of all financial entities
