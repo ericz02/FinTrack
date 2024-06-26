2bf991dc3818fe0ea9a84ff2bdee0c1d2edd0045
ericz02 <ezheng0720@gmail.com>
2024-06-26 11:16:39 -0400

    port

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8000,
+  },
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
 });
